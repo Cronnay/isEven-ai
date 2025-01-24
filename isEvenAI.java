657b9a7f39cc5e61725737f94693773827a4648c
philipwa <philipwa@student.chalmers.se>
2025-01-24 20:03:28 +0100

    IsEvenAI

---
+++ isEvenAI.java
@@ -0,0 +1,8 @@
+public class isEvenAI {
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("Please Give 1 numbers as argument!");
+        }
+        System.out.println("It cannot be determined whether " + args[0] + " is even or not");
+    }
+}
