e1b5ddba08c29e2dfe19ce25dc5f052b0abbf137
Sebastian Berglönn <sebastian.berglonn@cygni.se>
2025-01-24 19:55:23 +0100

    updated

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 import Login from './components/Login.vue'
+import Voucher from './components/Voucher.vue';
+
+const loginDone = ref(false);
 </script>
 
 <template>
   <div class="w-screen h-screen bg-white">
-    <Login />
+    <Login @done="loginDone = true" v-if="!loginDone" />
+    <Voucher v-if="loginDone" />
     <HelloWorld msg="Vite + Vue" />
   </div>
 </template>
--- web/src/components/Voucher.vue
+++ web/src/components/Voucher.vue
@@ -7,14 +7,16 @@ const textInput = ref("")
 const correctInput = ref(false)
 const submitted = ref(false)
 
-function voucherCheck() {
+const cardText = ref("");
 
-    if (textInput.value == "abba") {
-        correctInput.value = true
-    } else {
-        correctInput.value = false
-    }
-    submitted.value = true
+
+function voucherCheck() {
+  if (textInput.value == "abba") {
+    correctInput.value = true
+  } else {
+    correctInput.value = false
+  }
+  submitted.value = true
 
 }
 
@@ -24,7 +26,7 @@ function voucherCheck() {
 
   <div v-if="!correctInput" class="flex align-center justify-center">
     <form class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        
+
       <div class="mb-4">
         <label for="Voucher input" class="block text-sm font-medium text-gray-700">Input Voucher</label>
         <input v-model="textInput" type="text" id="voucherInput" name="voucherInput" placeholder=""
@@ -34,7 +36,7 @@ function voucherCheck() {
       <button @click.prevent="voucherCheck()" type="submit"
         class="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         Submit
-      </button>  
+      </button>
 
       <div v-if="submitted && !correctInput" class="mb-4">
         <p class="pt-2">Voucher wrong</p>
@@ -43,14 +45,6 @@ function voucherCheck() {
     </form>
   </div>
   <div v-if="correctInput" class="flex align-center justify-center">
-    <form class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-
-        <div class="mb-4">
-            <div class=""></div>
-        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-        <input type="email" id="email" name="email" placeholder="Your Email"
-          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-      </div>
-    </form>
+    <h1 class="text-3xl">Download our desktop application</h1>
   </div>
 </template>
